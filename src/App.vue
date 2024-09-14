b27c68471544132d2e65febbb5c02826dccf72e4
Markus Olsson <Markus.Olsson.Extern@skanetrafiken.se>
2024-09-14 22:29:44 +0200

    initial commit.

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <main>
+    </main>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<script setup>
+</script>
+
+<template>
+  <main class="bg">
+    <div class="container">
+      <div class="text-center pt-5">
+        <h1>Delirio</h1>
+        <h2>Diana Peralta</h2>
+      </div>
+    </div>
+  </main>
+</template>
